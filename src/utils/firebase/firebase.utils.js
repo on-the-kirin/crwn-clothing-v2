4f0bd43c72fdceafefcf0779e7446c62a46f10d1
on-the-kirin <onthekirin@naver.com>
2023-12-03 12:48:00 +0900

    파이어베이스 및 회원가입 기능  추가

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  Firestore,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjsVWDu1UQLEZxITzhE4J0io9FpC5KSAY",
+  authDomain: "crwn-clothing-db-ba3cf.firebaseapp.com",
+  projectId: "crwn-clothing-db-ba3cf",
+  storageBucket: "crwn-clothing-db-ba3cf.appspot.com",
+  messagingSenderId: "714371076835",
+  appId: "1:714371076835:web:eafa672ceffb4cf80febd6",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("error, creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
